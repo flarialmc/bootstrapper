340fc11dc7ba3c41df7730959f75347f7801ed12
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-12-15 10:00:36 +0530

    release: Update actions file.

--- Build.cmd
+++ Build.cmd
@@ -1,18 +1,11 @@
 @echo off
 cd "%~dp0/src"
 
-echo Clearing binaries...
 rd /q /s "bin"
 md "bin"
 
-echo Clearing objects...
 rd /q /s "obj"
 md "obj"
 
-echo Building objects...
 windres.exe -i "Resources\Library.rc" -o "obj\Library.o"
-
-echo Building artifacts...
 gcc.exe -Oz -s -Wl,--gc-sections,--exclude-all-symbols,--wrap=memcpy,--wrap=memset -static -shared -municode -nostdlib -e DllMain -DINITGUID "Library.c" "obj\Library.o" -lminhook -lkernel32 -luser32 -ldxgi -o "bin\vcruntime140_1.dll"
-
-echo %~dp0/src/bin/vcruntime140_1.dll
