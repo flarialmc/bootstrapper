069e3a4ad1f2b957f19773fdbf468f2b48121fa2
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-12-27 15:25:35 +0530

    release: Test.

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,5 @@
 @echo off
-cd "%~dp0/src"
+cd "%~dp0\src"
 
 rd /q /s "bin"
 md "bin"
@@ -10,4 +10,4 @@ md "obj"
 windres.exe -i "Resources\Library.rc" -o "obj\Library.o"
 gcc.exe -Oz -s -Wl,--gc-sections,--exclude-all-symbols,--wrap=memcpy,--wrap=memset -static -shared -municode -nostdlib -e DllMain -DINITGUID "Library.c" "obj\Library.o" -lminhook -lkernel32 -luser32 -ldxgi -o "bin\vcruntime140_1.dll"
 
-echo "%~dp0/bin/vcruntime140_1.dll"
+echo "%~dp0\bin\vcruntime140_1.dll"
